fcb90f7d7e18d1587eac6b459e8077def9b2dc8c
Gabriel Gomes <gabriel.gomes.silva.999@gmail.com>
2019-08-30 09:40:27 -0300

    Iniciado o exercicio de lista linear encadeada

---
+++ ListaLinearEncadeada/Entities/ArrayEntitie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaLinearEncadeada.Entities
+{
+    public class ArrayEntitie
+    {
+        public int[] Array { get; private set; }
+        private readonly int Inicio = 0;
+        private int Final;
+
+        public ArrayEntitie(int lenght)
+        {
+            Final = lenght;
+            Array = new int[lenght];
+        }
+
+        public int[] AdicionarElementoArray()
+        {
+            for(int i = 0; i < Final; i++)
+            {
+                Console.Write($"Digite o {Inicio+1}° elemento da array: ");
+                Array[i] = int.Parse(Console.ReadLine());
+            }
+
+            return Array;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < Final; i++)
+            {
+                sb.AppendLine(Array[i].ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ListaLinearEncadeada/Program.cs
@@ -0,0 +1,18 @@
+﻿using ListaLinearEncadeada.Entities;
+using System;
+
+namespace ListaLinearEncadeada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o Tamanho do vetor: ");
+            ArrayEntitie array = new ArrayEntitie(int.Parse(Console.ReadLine()));
+
+            array.AdicionarElementoArray();
+
+            Console.WriteLine(array);
+        }
+    }
+}
